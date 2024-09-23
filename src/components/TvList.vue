d122df69540e718fbdb0b435dc2abb1116a0b1b9
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-23 13:25:50 +0200

    add tv poster

--- src/components/TvList.vue
+++ src/components/TvList.vue
@@ -3,6 +3,7 @@
     <h2>Risultati Serie TV</h2>
     <ul>
       <li v-for="(tvShow, index) in tvShows" :key="index">
+        <img :src="getImageUrl(tvShow.poster_path)" alt="Copertina" width="200px" />
         <h3>{{ tvShow.name }}</h3>
         <p><strong>Titolo Originale: </strong> {{ tvShow.original_name }}</p>
 
@@ -17,14 +18,12 @@
           <span v-else>Non disponibile</span>
         </p>
 
-        <p><strong>Voto:</strong> {{ tvShow.vote_average }}</p>
-
-        <div>
-          <span v-for="star in 5" :key="star" class="star">
-            <i v-if="star <= getStarRating(tvShow.vote_average)" class="fas fa-star stars"></i>
-            <i v-else class="far fa-star stars"></i>
+        <p><strong>Voto:</strong>
+          <span class="stars">
+            <i v-for="star in getStarRating(tvShow.vote_average)" :key="'filled-' + star" class="fas fa-star"></i>
+            <i v-for="star in 5 - getStarRating(tvShow.vote_average)" :key="'empty-' + star" class="far fa-star"></i>
           </span>
-        </div>
+        </p>
       </li>
     </ul>
   </div>
@@ -33,6 +32,9 @@
 <script>
 import { useFlag } from '../composables/useFlag';
 import { useRating } from '../composables/useRatings';
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+const IMAGE_SIZE = 'w342';
 
 export default {
   name: 'TvList',
@@ -43,9 +45,14 @@ export default {
     const { getFlagUrl } = useFlag();
     const { getStarRating } = useRating();
 
+    function getImageUrl(posterPath) {
+      return `${IMAGE_BASE_URL}${IMAGE_SIZE}${posterPath}`;
+    }
+
     return {
       getFlagUrl,
-      getStarRating
+      getStarRating,
+      getImageUrl
     };
   }
 };
